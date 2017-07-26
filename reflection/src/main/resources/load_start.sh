10910a39eecffc9152e670845425c1c8cfdc1aa9
aattuluri <AnilKumar_Attuluri@intuit.com>
2017-07-26 07:37:53 -0700

    Make each curl request a background task

--- reflection/src/main/resources/load_start.sh
+++ reflection/src/main/resources/load_start.sh
@@ -5,7 +5,7 @@ echo "Running with TPS: $tps"
 
 while [ $ctr -le $tps ]
 do
-  curl -s http://localhost:10081/test?[1-10000000] &> /dev/null
+  curl -s http://localhost:10081/test?[1-10000000] &> /dev/null &
   pidlist="$pidlist $!"
   ((ctr++))
 done
